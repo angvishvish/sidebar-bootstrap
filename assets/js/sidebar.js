45531d565db9fee2d294ebf584e2f4723aab4ecd
angvishvish <angvish@promobitech.com>
2015-03-20 21:27:18 +0530

    adding less styling

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,21 +1,7 @@
 (function() {
   $(".set-layout").click(function(e) {
     e.preventDefault();
-    var position = $(".layout-pos").val();
-    if (position === 'right') {
-      $('.main-wrapper').removeClass('toggled-left').addClass('toggle-' + position);
-      $('.sidebar-toggle').removeClass('sidebar-toggle-left').addClass('sidebar-toggle-' + position);
-      $('.sidebar-wrapper-left').removeClass('sidebar-wrapper-left').addClass('sidebar-wrapper-' + position);  
-    } else {
-      $('.main-wrapper').removeClass('toggled-right').addClass('toggle-' + position);
-      $('.sidebar-toggle').removeClass('sidebar-toggle-right').addClass('sidebar-toggle-' + position);
-      $('.sidebar-wrapper-right').removeClass('sidebar-wrapper-right').addClass('sidebar-wrapper-' + position);
-    }
     
-    $(".sidebar-toggle-right").click(function(e) {
-        e.preventDefault();
-        $(".main-wrapper").toggleClass("toggled-right");
-    });
   });
 
   $(".sidebar-toggle-left").click(function(e) {
@@ -23,5 +9,25 @@
     $(".main-wrapper").toggleClass("toggled-left");
   });
 
-  
 })();
+
+function select_layout () {
+  var select_layout = document.getElementById("selected-dropdown");
+  position = select_layout.options[select_layout.selectedIndex].value;
+  
+  if (position === 'right') {
+    $('.main-wrapper').removeClass('toggled-left').addClass('toggle-' + position);
+    $('.sidebar-toggle').removeClass('sidebar-toggle-left').addClass('sidebar-toggle-' + position);
+    $('.sidebar-wrapper-left').removeClass('sidebar-wrapper-left').addClass('sidebar-wrapper-' + position);  
+  } else {
+    $('.main-wrapper').removeClass('toggled-right').addClass('toggle-' + position);
+    $('.sidebar-toggle').removeClass('sidebar-toggle-right').addClass('sidebar-toggle-' + position);
+    $('.sidebar-wrapper-right').removeClass('sidebar-wrapper-right').addClass('sidebar-wrapper-' + position);
+  }
+  
+  $(".sidebar-toggle-right").click(function(e) {
+    e.preventDefault();
+    $(".main-wrapper").toggleClass("toggled-right");
+  });
+
+}
